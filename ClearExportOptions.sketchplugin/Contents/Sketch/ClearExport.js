9ace4f04df8353fc01a0568ba0ac31c625b89a21
Emre C. Coskun <emrerussia@gmail.com>
2018-11-14 23:30:05 +0300

    Update to v2.0
    - Supports Sketch 52.x
    - Now shows number of assets that export options are cleared.
    - Added refreshLayerListIfNecessary to the end of the process.

--- ClearExportOptions.sketchplugin/Contents/Sketch/ClearExport.js
+++ ClearExportOptions.sketchplugin/Contents/Sketch/ClearExport.js
@@ -3,31 +3,30 @@
 var onRun = function(context) {
 
   var doc = context.document;
-  doc.currentPage().deselectAllLayers();
+  var page = doc.currentPage();
+  page.changeSelectionBySelectingLayers(nil);
   var pages = [doc pages];
   //var artboards = [[doc currentPage] artboards];
   var pagesCount = pages.count();
   var artboardsCount = 0;
   var exportablesCount = 0;
-
   //loop through the pages
   for (var i = 0; i < pages.count(); i++){
     //reference each page
     var page = pages[i];
     var exportables = page.exportableLayers();
-    exportablesCount =  exportablesCount + exportables.length;
-    for (var j = 0; j < exportables.length; j++) {
+    var exportablesLength = exportables.length;
+    for (var j = 0; j < exportablesLength; j++) {
       var exportable = exportables.objectAtIndex(j);
       if (exportable instanceof MSArtboardGroup) {
         //log("ARTBOARDS: " + exportable.name());
       } else {
         exportable.exportOptions().removeAllExportFormats();
+        exportablesCount =  exportablesLength;
         //log("ELEMENTS: " + exportable.name());
       }
-
     }
-
   }
-  //log(exportablesCount);
- [doc showMessage: "Sweet! Elements in all " + pagesCount + " pages are not exportable anymore. ✌️ (Changed mind? Use ⌘Z)"];
+ doc.refreshLayerListIfNecessary(nil);
+ [doc showMessage: "Sweet! " + exportablesCount + " elements in all " + pagesCount + " pages are not exportable anymore. ✌️ (Changed mind? Use ⌘Z)"];
 }
